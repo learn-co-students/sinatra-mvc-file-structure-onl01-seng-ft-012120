b7f37248780e75e64307bf75e376cd5bff106b80
kellypowers <kpowers.nj@gmail.com>
2020-02-20 18:46:06 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,3 +9,18 @@ class ApplicationController < Sinatra::Base
   	erb :index
   end
 end
+
+class Dog 
+  @@all = []
+  attr_accessor :name, :breed, :age
+  def initialize(name, breed, age)
+    @name = name
+    @breed = breed 
+    @age = age
+    @@all << self
+  end
+
+  def self.all
+    @@all 
+  end
+end
